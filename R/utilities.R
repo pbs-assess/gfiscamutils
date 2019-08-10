e323fd1d798df929f43cbf786e88b7aa58d493a5
Chris Grandin <cgrandin@shaw.ca>
2019-08-10 07:02:38 -0700

    Remove herring specific functions

--- R/utilities.R
+++ R/utilities.R
@@ -443,22 +443,6 @@ PasteNicely <- function( x, intChars=", ", nChar="and " ) {
   return( res )
 }  # End PasteNicely function
 
-#' Function to add a new column indicating group ID based on sequential data
-#'
-#' @param vec the vector
-#'
-#' @return a vector where the ID depends on whether the value of x is
-#'  sequential. For example, indicate whether a series of years is sequential,
-#'  or if there are say three groups of sequential years.
-#'  Break up the data by groups with consecutive values
-consecutive_group <- function(vec) {
-  d_unique_groups <- split(x = vec, f = cumsum(c(1, diff(vec) != 1)))
-  for(g in seq_along(d_unique_groups)){
-    d_unique_groups[[g]] <- rep(g, times = length(d_unique_groups[[g]]))
-  }
-  as.vector(unlist(d_unique_groups))
-}
-
 # Function to add a new column indicating the number of consecutive values
 CountConsecutive <- function( vec ) {
   # Determine the number of consecutive values in a vector. For example,
