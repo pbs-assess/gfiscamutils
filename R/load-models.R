332f38ad156e30c419cd1ddef87655762d1a2f35
Chris Grandin <chris.grandin@gmail.com>
2021-07-04 01:14:29 -0700

    Add delete_files_ext() * Delete files with as certain extension recursively

--- R/load-models.R
+++ R/load-models.R
@@ -601,6 +601,29 @@ calc.probabilities <- function(model,
   proj.dat
 }
 
+#' Delete all files with a specific extension recursively. By default, .rds files
+#'
+#' @param path The directory to start in.
+#' @param ext The extension of files to include for deletion
+#' @export
+delete_files_ext <- function(path = NULL, ext = "rds"){
+  stopifnot(!is.null(path))
+  ext_full <- paste0(".", ext, "$")
+  ans <- readline(paste0("Warning - you are about to delete all ", ext, " files recursively\n",
+                         "in the ", path, " directory. You cannot undo this operation.\n",
+                         "Are you sure? (y/n) <ENTER> "))
+  if(ans == "n"){
+    message("No files were deleted.")
+    return(invisible())
+  }
+  dirs <- file.path(list.dirs(path, recursive = TRUE))
+  map(dirs, ~{
+    unlink(file.path(.x, grep(ext_full, dir(.x), value = TRUE)))
+  })
+  message("Deleted all ", ext, " files.")
+  invisible()
+}
+
 #' Delete .Rdata files from all model directories and the retrospective subdirectories.
 #'   Herring has AM2 as a subdirectory of each model
 #'
