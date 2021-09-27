7a47ba59499d05b4bb57dc7d894e4bb7abc3cba5
Chris Grandin <chris.grandin@gmail.com>
2021-09-27 00:50:10 -0700

    Improve residuals plot code

--- R/figures-age.R
+++ R/figures-age.R
@@ -5,6 +5,7 @@
 #' @param gear The gear number to plot
 #' @param type One of 'bars' or 'pearson'
 #' @param pearson_type One of 'age', 'year', or 'birthyear'
+#' @param angle_x_labels If `TRUE` put 45 degree angle on x-axis tick labels
 #'
 #' @return a [ggplot2::ggplot()] object
 #' @importFrom ggplot2 geom_linerange facet_grid
@@ -13,7 +14,8 @@ plot_agecomp_fits_splitsex <- function(model,
                                        gear,
                                        model_name = "",
                                        type = "bars",
-                                       pearson_type = "age"){
+                                       pearson_type = "age",
+                                       angle_x_labels = FALSE){
 
   if(!type %in% c("bars", "pearson")){
     stop("type must be one of 'bars' or 'pearson'",
@@ -66,43 +68,96 @@ plot_agecomp_fits_splitsex <- function(model,
       rename(`Standardized Residuals` = Proportion)
 
     if(pearson_type == "age"){
-      g <- ggplot(resids, aes(Age, `Standardized Residuals`, fill = Sex)) +
-        stat_boxplot(geom = "errorbar") +
-        geom_boxplot(outlier.colour = "black",
-                     outlier.shape = 3,
-                     outlier.size = 0.5) +
-        geom_hline(aes(yintercept = 0), color = "red", linetype = "dashed", size = 0.3) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 0.55, vjust = 0.5),
-              plot.title = element_text(size = 14, face = "bold")) +
-        ggtitle(model_name) +
-        ylab("")
+      aes_x <- "Age"
     }else if(pearson_type == "year"){
-      g <- ggplot(resids, aes(Year, `Standardized Residuals`, fill = Sex)) +
-        stat_boxplot(geom = "errorbar") +
-        geom_boxplot(outlier.colour = "black",
-                     outlier.shape = 3,
-                     outlier.size = 0.5) +
-        geom_hline(aes(yintercept = 0), color = "red", linetype = "dashed", size = 0.3) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 0.55, vjust = 0.5),
-              plot.title = element_text(size = 14, face = "bold")) +
-        ggtitle(model_name) +
-        ylab("")
+      aes_x <- "Year"
     }else if(pearson_type == "birthyear"){
-      resids <- resids %>% mutate(`Year of birth` = factor(as.numeric(as.character(Year)) - as.numeric(as.character(Age)))) %>%
+      resids <- resids %>%
+        mutate(`Year of birth` = factor(as.numeric(as.character(Year)) - as.numeric(as.character(Age)))) %>%
         select(-Year)
-      g <- ggplot(resids, aes(`Year of birth`, `Standardized Residuals`, fill = Sex)) +
-        stat_boxplot(geom = "errorbar") +
-        geom_boxplot(outlier.colour = "black",
-                     outlier.shape = 3,
-                     outlier.size = 0.5) +
-        geom_hline(aes(yintercept = 0), color = "red", linetype = "dashed", size = 0.3) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 0.55, vjust = 0.5),
-              plot.title = element_text(size = 14, face = "bold")) +
-        ggtitle(model_name) +
-        ylab("")
+      aes_x <- "`Year of birth`"
+    }
+    g <- ggplot(resids, aes_string(aes_x, "`Standardized Residuals`", fill = "Sex")) +
+      stat_boxplot(geom = "errorbar") +
+      geom_boxplot(outlier.colour = "black",
+                   outlier.shape = 3,
+                   outlier.size = 0.5) +
+      scale_fill_manual(values = c("#FF000050", "#0000FF50")) +
+      geom_hline(aes(yintercept = 0), color = "red", linetype = "dashed", size = 0.3) +
+      theme(plot.title = element_text(size = 14, face = "bold")) +
+      ggtitle(model_name) +
+      ylab("")
+
+    if(angle_x_labels){
+      g <- g + theme(axis.text.x = element_text(angle = 45, hjust = 0.55, vjust = 0.5))
     }
   }
   g
+}
+
+
+#' Plot a three-panel residual figure. The panels are Age, Year, and Birth year residuals
+#'
+#' @param model An iSCAM model
+#' @param model_name Name of the model for plot title
+#' @param gear Number of gear to plot
+#' @param gear_name Name of the gear for plot label
+#' @param ylim_age ylim for the Age plot
+#' @param ylim_year ylim for the Year plot
+#' @param ylim_birthyear ylim for the birthyear plot
+#' @param legend_pos Position of the legend inside the plot area (two-element vector of x, y)
+#' @param show_title If `TRUE`, show the title (model and gear names) on top of the three panels
+#' @param angle_x_labels A three-element vector of logicals. If `TRUE`, angle the x-axis tick
+#' labels for that plot, where index 1 = Age, 2 = Year, and 3 = Birthyear plots
+#'
+#' @return a [cowplot::plot_grid()] object
+#' @export
+#' @importFrom cowplot plot_grid
+plot_age_year_birthyear_residuals <- function(model,
+                                              model_name = "iSCAM model",
+                                              gear = 1,
+                                              gear_name = model$dat$age_gear_abbrevs[gear],
+                                              ylim_age = c(-1, 1),
+                                              ylim_year = c(-1, 1),
+                                              ylim_birthyear = c(-1, -1),
+                                              legend_pos = c(0.05, 0.15),
+                                              show_title = TRUE,
+                                              angle_x_labels = c(FALSE, FALSE, TRUE)){
+
+  plot_grid(
+    plot_agecomp_fits_splitsex(model,
+                               gear = gear,
+                               model_name = ifelse(show_title, paste(model_name, ", ", gear_name), ""),
+                               type = "pearson",
+                               pearson_type = "age",
+                               angle_x_labels = angle_x_labels[1]) +
+      coord_cartesian(ylim = ylim_age) +
+      theme(strip.text.x = element_blank(),
+            strip.background = element_rect(colour = "white", fill = "white"),
+            panel.border = element_rect(colour = "black", fill = NA),
+            legend.box.background = element_rect(colour = "black"),
+            legend.position = legend_pos),
+
+    plot_agecomp_fits_splitsex(model,
+                               gear = gear,
+                               type = "pearson",
+                               pearson_type = "year",
+                               angle_x_labels = angle_x_labels[2]) +
+      ylab("Standardized Residuals") +
+      theme(axis.title.y = element_text(size = 24)) +
+      coord_cartesian(ylim = ylim_year) +
+      theme(legend.position = "none"),
+
+    plot_agecomp_fits_splitsex(model,
+                               gear = gear,
+                               type = "pearson",
+                               pearson_type = "birthyear",
+                               angle_x_labels = angle_x_labels[3]) +
+      coord_cartesian(ylim = ylim_birthyear) +
+      theme(legend.position = "none"),
+
+    ncol = 1,
+    align = "v")
 }
 
 #' Plot age comp estimates
