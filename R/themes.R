d71faa5f05a420db4c5c6db970ccbfa7b247a92a
Chris Grandin <chris.grandin@gmail.com>
2022-04-09 00:15:36 -0700

    Add gfiscam theme

--- R/themes.R
+++ R/themes.R
@@ -1,18 +1,23 @@
-#' ggplot theme for Herring documents
+#' ggplot theme for gfiscam
 #'
 #' @return a customized [ggplot2::theme()]
-#' @importFrom ggplot2 theme theme_bw element_rect margin element_blank element_line alpha unit
+#' @importFrom ggplot2 theme theme_set theme_bw element_rect margin element_blank element_line alpha unit
 #' @export
-herring_theme <- function(){
+gfiscam_theme <- function(){
   theme_bw() +
   theme(legend.box.background = element_rect(fill = alpha("white", 0.7)),
         legend.box.margin = margin(1, 1, 1, 1, "mm"),
         legend.key = element_blank(),
         legend.margin = margin(),
-        legend.text.align = 1,
-        panel.grid.major = element_line(colour = "darkgrey", size = 0.2),
-        panel.grid.minor = element_line(colour = "darkgrey", size = 0.1),
+        legend.text.align = 0,
         legend.background = element_rect(fill = "transparent"),
+        #strip.background = element_rect(fill = "transparent"),
+        strip.background = element_blank(),
+        #strip.text.x = element_text(face = "bold", color = "black"),
         #panel.spacing.x=unit(3, "lines"),
-        plot.margin = unit(c(0.1, 0.6, 0.1, 0.1), "lines"))
+        plot.margin = unit(c(0.1, 0.6, 0.1, 0.1), "lines"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(color = "black", fill = NA, size = 1)
+        )
 }
