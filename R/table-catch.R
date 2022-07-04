bdd8a5fcb751ecbbd26cb0f9e5bc1d1c0eff9d35
Chris Grandin <chris.grandin@gmail.com>
2022-07-04 10:55:52 -0700

    Modify table_catch()

--- R/table-catch.R
+++ R/table-catch.R
@@ -2,18 +2,10 @@
 #'
 #' @param catch_df A data frame as output by [gfplot::tidy_catch()]
 #' @param start_yr The year to start the table or `NULL` to include all data
-#' @param avail_gears If `TRUE`, return a vector of the names of gears you can
-#' provide to `gear_rm`. If `FALSE`, return the [csasdown::csas_table()]
-#' @param gear_rm A vector of gear types to remove from the table. Call this
-#' function with `avail_gears` set to `TRUE` for a listing of available gears
+#' @param area If `NULL`, sum together all catch by year for all areas found
+#' in `catch_df`. Otherwise, a single area name (eg. 3CD or 5ABCDE). If the
+#' area name is not in `catch_df$area`, an error will be thrown
 #' @param gear_col_widths Width to make the columns for the gear types
-#' @param by_gear If `TRUE` the table will contain a column for each gear
-#' except for those found in `gear_rm`. If `FALSE` only a total column will be
-#' shown, unless `by_area` is `TRUE`, then a total column for each area will
-#' be shown
-#' @param by_area If `TRUE`, the table will contain a column for each area
-#' except for those found in `area_rm`. If `FALSE` only a total column will be
-#' shown, unless `by_gear` is `TRUE`
 #' @param scale_factor Value to divide all the catches by to make the table
 #' in tonnes
 #' @param ... Arguments to pass to [csasdown::csas_table()]
@@ -22,41 +14,37 @@
 #' @export
 table_catch <- function(catch_df,
                         start_yr = NULL,
-                        avail_gears = FALSE,
-                        gear_rm = available_gears,
+                        area = NULL,
                         gear_col_widths = "5em",
                         scale_factor = 1e3,
                         ...){
 
-  available_gears <- c("Bottom trawl",
-                       "Discarded",
-                       "Unknown/trawl",
-                       "Midwater",
-                       "trawl",
-                       "Hook and line",
-                       "LONGLINE",
-                       "Trap")
+  if(length(unique(catch_df$species_common_name)) > 1){
+    stop("There is more than one species in `catch_df`", call. = FALSE)
+  }
+  catch_df <- catch_df |>  select(-species_common_name)
 
-  if(avail_gears){
-    return(available_gears)
+
+  if(is.null(area)){
+    catch_df <- catch_df |>
+      group_by(year, gear) |>
+      summarize(value = sum(value))
+  }else{
+    if(length(area) > 1){
+      stop("`area` must either be `NULL` or a single area name. You have ",
+           "supplied more than one name.",
+           call. = FALSE)
+    }
+    if(!area %in% catch_df$area){
+      stop("`area` = '", area, "' was not found in `catch_df`",
+           call. = FALSE)
+    }
+    catch_df <- catch_df |>
+      filter(area == !!area)
   }
 
-  if(!all(gear_rm %in% available_gears)){
-    stop("Not all `gear_rm` are in the list of available gears. Call this ",
-         "function with `avail_gears` set to `TRUE` for a listing of ",
-         "available gears",
-         call. = FALSE)
-  }
-
-  if(length(unique(catch_df$area)) != 1){
-    stop("More than one area found in the `catch_df` data frame",
-         call. = FALSE)
-  }
-
-  if(length(unique(catch_df$species_common_name)) != 1){
-    stop("More than one species_common_name found in the `catch_df` data frame",
-         call. = FALSE)
-  }
+  catch_df <- catch_df |>
+    pivot_wider(names_from = "gear", values_from = "value")
 
   if(is.null(start_yr)){
     start_yr <- min(catch_df$year)
@@ -68,24 +56,22 @@ table_catch <- function(catch_df,
     }
   }
 
-  tab <- catch_df %>%
-    select(-area, -species_common_name) %>%
-    filter(!gear %in% gear_rm) %>%
-    filter(year >= start_yr) %>%
-    pivot_wider(names_from = "gear", values_from = "value") %>%
+  browser()
+  tab <- catch_df |>
+    filter(year >= start_yr) |>
     rename(Year = year)
-
+browser()
   # Add 'Total Landings' column
-  total <- tab %>%
-    select(-Year, -Discarded) %>%
-    mutate(total = rowSums(.)) %>%
-    select(total) %>%
+  total <- tab |>
+    select(-Year, -Discarded) |>
+    mutate(total = rowSums(.)) |>
+    select(total) |>
     rename(`Total Landings` = total)
-  tab <- tab %>%
+  tab <- tab |>
     bind_cols(total)
 
   # Convert values and swap last two cols
-  tab <- tab %>%
+  tab <- tab |>
     mutate_at(vars(-Year), ~{. / scale_factor})
   ord <- 1:ncol(tab)
   tmp_ord <- ord
@@ -107,7 +93,7 @@ table_catch <- function(catch_df,
                     ...)
 
   if(!is.null(gear_col_widths)){
-    out <- out %>%
+    out <- out |>
       column_spec(2:ncol(tab), width = gear_col_widths)
   }
 
