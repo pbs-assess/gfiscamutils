a040e9b04611c1e506651252e63a50f965e0380d
Chris Grandin <chris.grandin@gmail.com>
2022-09-02 00:58:06 -0700

    Add function to rename a gear in the iscam data file

---
+++ R/rename_gear_iscam.R
@@ -0,0 +1,28 @@
+#' Read in an ISCAM data file, replce all instances of the old name
+#' with the new name and save the file, overwriting it
+#'
+#' @param fn The iSCAM data file
+#' @param old_gear_name The old name or abbreviation for the gear/fleet
+#' @param new_gear_name The new name or abbreviation for the gear/fleet
+#'
+#' @return Nothing
+#' @export
+rename_gear_iscam <- function(fn,
+                              old_gear_name = NULL,
+                              new_gear_name = NULL){
+
+  if(!file.exists(fn)){
+    stop("File `", fn, "` does not exist", call. = FALSE)
+  }
+  d <- readLines(fn)
+  if(!any(grepl(old_gear_name, d))){
+    message("The `old_gear_name` = ", old_gear_name, " was not found in the ",
+            "file `", fn, "`")
+  }else{
+    d <- gsub(old_gear_name, new_gear_name, d)
+    writeLines(d, fn)
+    message("The `old_gear_name` = ", old_gear_name, " was found and ",
+            "renamed in the file `", fn, "`")
+  }
+  invisible()
+}
