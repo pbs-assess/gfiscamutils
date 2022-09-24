ecfc8c18546ac344397d04697a49d8d3bd9524fc
Chris Grandin <chris.grandin@gmail.com>
2022-09-23 23:25:22 -0700

    Add catch-by-fleet plotting function

---
+++ R/plot-catch-fleet.R
@@ -0,0 +1,15 @@
+#' Plot catch by fleet
+#'
+#' @param catch_lst A list of catch data frames, as output from
+#' [gfdata::get_catch()]
+#' @param ... Arguments passed to [gfplot::plot_catch()]
+#'
+#' @return Nothing
+#' @export
+plot_catch_fleet <- function(catch_lst, fleet_nms, ...){
+  ct <- map2_dfr(catch_lst, fleet_nms, ~{
+    .x |>
+      mutate(area = .y)
+  })
+  plot_catch(ct, ...)
+}
