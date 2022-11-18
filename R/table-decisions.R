70df565509d9370361b38191d4299dd80cf5cf68
Chris Grandin <chris.grandin@gmail.com>
2022-11-18 00:31:46 -0800

    Finish decision table

--- R/table-decisions.R
+++ R/table-decisions.R
@@ -5,8 +5,11 @@
 #'
 #' @param model An iscam model object (class [mdl_cls])
 #' @param format See `format` in [knitr::kable()]
-#' @param bo_refpts Vector of two proportional values for the limit reference
-#' point and Upper stock reference. Values are 0.2B0 and 0.4B0 by default
+#' @param bo_refpts Vector of proportional values for the reference
+#' points
+#' @param num_proj_yrs The number of projection years to show in the table.
+#' The output may have more but only this many will be shown. If `NULL`,
+#' all projection years will be shown
 #' @param digits Number of decimal places for the values in the table
 #' @param ret_df Logical. If `TRUE` return a data frame with the values,
 #' instead of the [knitr::kable()] formatted table
@@ -18,7 +21,8 @@
 #' @importFrom gfutilities f
 table_decisions <- function(model,
                             format = c("latex", "html"),
-                            bo_refpts = c(0.2, 0.4),
+                            bo_refpts = c(0.2, 0.35, 0.4),
+                            num_proj_yrs = 3,
                             digits = 2,
                             ret_df = FALSE,
                             col_widths = NULL,
@@ -42,6 +46,12 @@ table_decisions <- function(model,
     }
   }
 
+  esc_open_prob <- ifelse(format == "html", "\\(P(", "P(")
+  esc_bo <- ifelse(format == "html", "B_{0}", "B\\textsubscript{0}")
+  esc_open_b <- ifelse(format == "html", "B_{", "B\\textsubscript{")
+  esc_close_b <- "}"
+  esc_close_prob <- ifelse(format == "html", "\\)", ")")
+
   ctl_options <- model$proj$ctl.options %>%
     as_tibble(rownames = "variable") %>%
     rename(value = V1)
@@ -59,69 +69,85 @@ table_decisions <- function(model,
   }) |>
     setNames(paste0(bo_refpts, "_bo"))
 
-  proj <- model$mcmccalcs$proj
-  proj_yrs_inds <- grep("^B20[0-9]+$", names(proj[[1]]))
-  proj_yrs_minus_first_inds <- proj_yrs_inds[-1]
+  # Filter out the columns `catch` and the years we need for the table calcs
+  # Make all biomass column names numeric (remove the preceeding 'B')
+  # Calculate depletion for all biomass columns
+  get_proj <- function(first_yr){
+    proj <- model$mcmccalcs$proj |>
+      imap(~{
+        if(is.null(num_proj_yrs)){
+          cols_to_keep <- "^catch$|^B20[0-9]+$"
+          cols_to_keep_inds <- grep(cols_to_keep, names(.x))
+          if(!length(cols_to_keep_inds)){
+            stop("The regular expression '", cols_to_keep, "' failed to match any column names ",
+                 "in the data frame for catch level ", .y)
+          }
+        }else{
+          cols_to_keep <- paste0("^catch$|",
+                                 paste0("^B",
+                                        first_yr:(model$dat$end.yr + num_proj_yrs + 2),
+                                        "$",
+                                        collapse = "|"))
+          cols_to_keep_inds <- grep(cols_to_keep, names(.x))
+          if(!length(cols_to_keep_inds)){
+            stop("The regular expression '", cols_to_keep, "' failed to match any column names ",
+                 "in the data frame for catch level ", .y)
+          }
+        }
+        output <- .x |> select(cols_to_keep_inds)
+        names(output) <- gsub("^B", "", names(output))
+        # Calculate depletion
+        output |> select(-catch) |> mutate_all(~{.x / sbo})
+      })
+  }
 
-  esc_open_prob <- ifelse(format == "html", "\\(P(", "P(")
-  esc_bo <- ifelse(format == "html", "B_{0}", "B\\textsubscript{0}")
-  esc_open_b <- ifelse(format == "html", "B_{", "B\\textsubscript{")
-  esc_close_b <- "}"
-  esc_close_prob <- ifelse(format == "html", "\\)", ")")
+  proj <- get_proj(model$dat$end.yr + 2)
+  # Calculate all probability columns for reference points
+  ptab <- map(bo_refpts, function(refpt){
+    j <- imap(proj, function(proj_tbl, catch_lvl){
+      # For each projection table at a given catch level ...
+      cols <- proj_tbl |>
+        map_dbl(~{
+          sum(.x < refpt) / length(.x)
+        })
+      names(cols) <- names(cols) |>
+        map_chr(~{
+          paste0(esc_open_prob, esc_open_b, .x, "} < ",
+                 refpt, esc_bo, esc_close_prob)
+        })
+      cols |>
+        vec2df(nms = names(cols))
+    }) |>
+      map_dfr(~{.x})
+  }) |>
+    map_dfc(~{.x})
 
-  tab <- imap(proj, ~{
-    # Make all columns numeric
-    x_tbl <- .x |>
-      as_tibble() |>
-      map_df(~{as.numeric(.x)})
-
-    # Calculate depletion for projected years
-    x <- x_tbl[proj_yrs_minus_first_inds] |> mutate_all(~{.x / sbo})
-    names(x) <- gsub("B", "", names(x))
-
-    i <- x |>
-      map_dbl(~{sum(.x < bo_refpts[1]) / length(.x)})
-    j <- x |>
-      map_dbl(~{sum(.x < bo_refpts[2]) / length(.x)})
-    # Calculate probability of decline for projected years
-
-    y <- x_tbl[proj_yrs_inds]
-    names(y) <- gsub("B", "", names(y))
-    k <- map2(y, seq_along(y), ~{
+  proj <- get_proj(model$dat$end.yr + 1)
+  # Calculate probability columns for dropping biomass year-to-year
+  j <- map(proj, function(proj_tbl){
+    # For each projection table at a given catch level ...
+    k <- proj_tbl |>
+      map2(seq_along(proj_tbl), ~{
       if(.y > 1){
-        sum(.x < y[[.y - 1]]) / length(.x)
+        sum(.x < proj_tbl[[.y - 1]]) / length(.x)
       }else{
         NULL
       }
     })
     k[lengths(k) == 0] <- NULL
     k <- k |> map_dbl(~{.x})
-
-    nms_fancy <- names(i) |>
-      map_chr(~{
-        paste0(esc_open_prob, esc_open_b, .x, "} < ",
-               bo_refpts[1], esc_bo, esc_close_prob)
-      })
-    names(i) <- nms_fancy
-
-    nms_fancy <- names(j) |>
-      map_chr(~{
-        paste0(esc_open_prob, esc_open_b, .x, "} < ",
-               bo_refpts[2], esc_bo, esc_close_prob)
-      })
-    names(j) <- nms_fancy
-
-    nms_fancy <- map(as.numeric(names(k)), ~{
-        paste0(esc_open_prob, esc_open_b, .x, "} < ",
-               esc_open_b, .x - 1, esc_close_b, esc_close_prob)
-      })
-    names(k) <- nms_fancy
-
-    out <- c(.x$catch[1], i, j, k)
-    names(out)[1] <- "Catch (thousand t)"
-    out
+    names(k) <- map(as.numeric(names(k)), ~{
+      paste0(esc_open_prob, esc_open_b, .x, "} < ",
+             esc_open_b, .x - 1, esc_close_b, esc_close_prob)
+    })
+    k
   }) |>
-    map_df(~{.x})
+    map_dfr(~{.x})
+
+  tab <- bind_cols(ptab, j) |>
+    mutate(catch = as.numeric(names(proj))) |>
+    select(catch, everything())
+  names(tab)[1] <- "Catch (thousand t)"
 
   if(ret_df){
     return(tab)
