08aff16a8f052cfeec0793f15208e0921698bc5f
Chris Grandin <chris.grandin@gmail.com>
2024-05-23 00:10:18 -0700

    Simplify gear translation function

--- R/set-gear-language.R
+++ R/set-gear-language.R
@@ -24,68 +24,34 @@ set_gear_language <- function(models, curr_gear_lang = NULL){
   gear_names <- base_model$dat$gear_names
   gear_abbrevs <- base_model$dat$gear_abbrevs
 
-  if(fr()){
-    if(curr_gear_lang == "en"){
-      # Translate all gear names to French
-      # allow_missing used here in case this code was already run, in which case
-      # translation is not necessary
-      gear_names_fr <- map_chr(gear_names, ~{
-        en2fr(.x, allow_missing = TRUE)
-      })
-      gear_abbrevs_fr <- map_chr(gear_abbrevs, ~{
-        en2fr(.x, allow_missing = TRUE)
-      })
+  gear_names_fr <- map_chr(gear_names, ~{
+    en2fr(.x, allow_missing = TRUE)
+  })
+  gear_abbrevs_fr <- map_chr(gear_abbrevs, ~{
+    en2fr(.x, allow_missing = TRUE)
+  })
+  gear_names_en <- map_chr(gear_names, ~{
+    fr2en(.x, allow_missing = TRUE)
+  })
+  gear_abbrevs_en <- map_chr(gear_abbrevs, ~{
+    fr2en(.x, allow_missing = TRUE)
+  })
 
-      # Base model
-      models[[1]][[1]] <-
-        replace_gear_names(models[[1]][[1]],
-                           old_gear_names = gear_names,
-                           new_gear_names = gear_names_fr,
-                           old_gear_abbrevs = gear_abbrevs,
-                           new_gear_abbrevs = gear_abbrevs_fr)
-      models$bridge_grps[[1]] <-
-        replace_gear_names(models$bridge_grps[[1]],
-                           old_gear_names = gear_names,
-                           new_gear_names = gear_names_fr,
-                           old_gear_abbrevs = gear_abbrevs,
-                           new_gear_abbrevs = gear_abbrevs_fr)
-      models$bridge_grps[[2]] <-
-        replace_gear_names(models$bridge_grps[[2]],
-                           old_gear_names = gear_names,
-                           new_gear_names = gear_names_fr,
-                           old_gear_abbrevs = gear_abbrevs,
-                           new_gear_abbrevs = gear_abbrevs_fr)
-    }
-  }else{
-    if(curr_gear_lang == "fr"){
-      gear_names_en <- map_chr(gear_names, ~{
-        fr2en(.x, allow_missing = TRUE)
-      })
-      gear_abbrevs_en <- map_chr(gear_abbrevs, ~{
-        fr2en(.x, allow_missing = TRUE)
-      })
-
-      # Base model
-      models[[1]][[1]] <-
-        replace_gear_names(models[[1]][[1]],
-                           old_gear_names = gear_names,
-                           new_gear_names = gear_names_en,
-                           old_gear_abbrevs = gear_abbrevs,
-                           new_gear_abbrevs = gear_abbrevs_en)
-      models$bridge_grps[[1]] <-
-        replace_gear_names(models$bridge_grps[[1]],
-                           old_gear_names = gear_names,
-                           new_gear_names = gear_names_en,
-                           old_gear_abbrevs = gear_abbrevs,
-                           new_gear_abbrevs = gear_abbrevs_en)
-      models$bridge_grps[[2]] <-
-        replace_gear_names(models$bridge_grps[[2]],
-                           old_gear_names = gear_names,
-                           new_gear_names = gear_names_en,
-                           old_gear_abbrevs = gear_abbrevs,
-                           new_gear_abbrevs = gear_abbrevs_en)
-    }
-  }
+  models <- models |>
+    map(\(grp){
+      grp |>
+        map(\(mdl_in_grp){
+          replace_gear_names(mdl_in_grp,
+                             old_gear_names = gear_names,
+                             new_gear_names = if(fr())
+                               gear_names_fr else
+                                 gear_names_en,
+                             old_gear_abbrevs = gear_abbrevs,
+                             new_gear_abbrevs = if(fr())
+                               gear_abbrevs_fr else
+                                 gear_abbrevs_en)
+                             })
+          })
 
   models
 }
